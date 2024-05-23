e7be1c5a573a425a1c02658e8e8867108081c52e
[madhan2023] <[madhankumarjoth@gmail.com]>
2024-05-23 22:37:26 +0530

    try my portfolio landing page

---
+++ src/pages/MyPortfolio.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import HeaderSection from "./landing_sections/HeaderSection";
+import AboutSection from "./landing_sections/AboutSection";
+import ProjectSection from "./landing_sections/ProjectSection";
+import ContactSection from "./landing_sections/ContactSection";
+import FooterSection from "./landing_sections/FooterSection";
+
+const MyPortfolio = () => {
+  return (
+    <>
+      <HeaderSection />
+      <AboutSection />
+      <ProjectSection />
+      <ContactSection />
+      <FooterSection />
+    </>
+  );
+};
+
+export default MyPortfolio;
